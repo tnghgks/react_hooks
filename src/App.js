6700fcf398893ed2c44079311b67b02d8d673155
HOSU HAN <tnghgks@naver.com>
2022-12-06 21:39:45 +0900

    🎉 프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import useInput from "./hooks/useInput";
 
 function App() {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
